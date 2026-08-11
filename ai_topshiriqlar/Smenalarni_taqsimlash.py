565235a425d1f967c23bcd4d02cba4016c85e7fe
mominjon4615-wq <mominjon4615@gmail.com>
2026-08-11 12:09:55 +0500

    Add AI task: Smenalarni taqsimlash

---
+++ ai_topshiriqlar/Smenalarni_taqsimlash.py
@@ -0,0 +1,10 @@
+# Smenalarni taqsimlash
+# Kurs: Dasturlash / IT
+# Mavzu: O'zgaruvchilar ⭐ — yaratish va nomlash qoidalari (snake_case)
+# Ball: 50
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+k = int(input())
+print(n // k)
+print(n % k)
